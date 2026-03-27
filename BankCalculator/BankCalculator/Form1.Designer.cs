8c229a3d1430218b6373c4c9439921d77fc1aa67
Dudka Oleksandr <120167950+Sl1ming@users.noreply.github.com>
2026-03-27 17:36:45 +0200

    Add files via upload

--- BankCalculator/BankCalculator/Form1.Designer.cs
+++ BankCalculator/BankCalculator/Form1.Designer.cs
@@ -37,91 +37,151 @@
             lblResult = new Label();
             btnCalculate = new Button();
             btnSave = new Button();
+            groupBox1 = new GroupBox();
+            rbCompound = new RadioButton();
+            rbSimple = new RadioButton();
+            rbLoan = new RadioButton();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(41, 45);
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Location = new Point(130, 133);
             label1.Name = "label1";
-            label1.Size = new Size(44, 20);
+            label1.Size = new Size(94, 25);
             label1.TabIndex = 0;
-            label1.Text = "Сума";
+            label1.Text = "Сума (грн)";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(189, 45);
+            label2.BorderStyle = BorderStyle.FixedSingle;
+            label2.Location = new Point(307, 133);
             label2.Name = "label2";
-            label2.Size = new Size(94, 20);
+            label2.Size = new Size(139, 25);
             label2.TabIndex = 1;
-            label2.Text = "Річна ставка";
+            label2.Text = "Річна ставка (%)";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(326, 45);
+            label3.BorderStyle = BorderStyle.FixedSingle;
+            label3.Location = new Point(496, 133);
             label3.Name = "label3";
-            label3.Size = new Size(79, 20);
+            label3.Size = new Size(90, 25);
             label3.TabIndex = 2;
             label3.Text = "Срок (міс)";
             // 
             // txtAmount
             // 
-            txtAmount.Location = new Point(37, 78);
+            txtAmount.Location = new Point(130, 171);
             txtAmount.Name = "txtAmount";
-            txtAmount.Size = new Size(90, 27);
+            txtAmount.Size = new Size(101, 30);
             txtAmount.TabIndex = 3;
             // 
             // txtTerm
             // 
-            txtTerm.Location = new Point(326, 78);
+            txtTerm.Location = new Point(496, 171);
             txtTerm.Name = "txtTerm";
-            txtTerm.Size = new Size(90, 27);
+            txtTerm.Size = new Size(101, 30);
             txtTerm.TabIndex = 4;
-            txtTerm.TextChanged += textBox2_TextChanged;
             // 
             // txtRate
             // 
-            txtRate.Location = new Point(189, 78);
+            txtRate.Location = new Point(307, 171);
             txtRate.Name = "txtRate";
-            txtRate.Size = new Size(90, 27);
+            txtRate.Size = new Size(110, 30);
             txtRate.TabIndex = 5;
             // 
             // lblResult
             // 
             lblResult.AutoSize = true;
-            lblResult.Location = new Point(539, 45);
+            lblResult.BorderStyle = BorderStyle.FixedSingle;
+            lblResult.Location = new Point(496, 242);
             lblResult.Name = "lblResult";
-            lblResult.Size = new Size(75, 20);
+            lblResult.Size = new Size(88, 25);
             lblResult.TabIndex = 6;
             lblResult.Text = "Результат";
             // 
             // btnCalculate
             // 
-            btnCalculate.Location = new Point(37, 155);
+            btnCalculate.BackColor = Color.DarkTurquoise;
+            btnCalculate.FlatAppearance.BorderSize = 0;
+            btnCalculate.FlatStyle = FlatStyle.Flat;
+            btnCalculate.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            btnCalculate.ForeColor = SystemColors.ControlText;
+            btnCalculate.Location = new Point(207, 403);
             btnCalculate.Name = "btnCalculate";
-            btnCalculate.Size = new Size(106, 29);
+            btnCalculate.Size = new Size(250, 52);
             btnCalculate.TabIndex = 7;
             btnCalculate.Text = "Розрахувати";
-            btnCalculate.UseVisualStyleBackColor = true;
+            btnCalculate.UseVisualStyleBackColor = false;
             btnCalculate.Click += btnCalculate_Click;
             // 
             // btnSave
             // 
-            btnSave.Location = new Point(185, 155);
+            btnSave.Location = new Point(496, 352);
             btnSave.Name = "btnSave";
-            btnSave.Size = new Size(94, 29);
+            btnSave.Size = new Size(106, 33);
             btnSave.TabIndex = 8;
             btnSave.Text = "Зберегти";
             btnSave.UseVisualStyleBackColor = true;
             btnSave.Click += btnSave_Click;
             // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(rbCompound);
+            groupBox1.Controls.Add(rbSimple);
+            groupBox1.Controls.Add(rbLoan);
+            groupBox1.Location = new Point(207, 233);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(250, 152);
+            groupBox1.TabIndex = 9;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Тип операції";
+            // 
+            // rbCompound
+            // 
+            rbCompound.AutoSize = true;
+            rbCompound.Location = new Point(0, 109);
+            rbCompound.Name = "rbCompound";
+            rbCompound.Size = new Size(242, 27);
+            rbCompound.TabIndex = 12;
+            rbCompound.TabStop = true;
+            rbCompound.Text = "Депозит (Складні відсотки)";
+            rbCompound.UseVisualStyleBackColor = true;
+            // 
+            // rbSimple
+            // 
+            rbSimple.AutoSize = true;
+            rbSimple.Location = new Point(0, 76);
+            rbSimple.Name = "rbSimple";
+            rbSimple.Size = new Size(233, 27);
+            rbSimple.TabIndex = 11;
+            rbSimple.TabStop = true;
+            rbSimple.Text = "Депозит (Прості відсотки)";
+            rbSimple.UseVisualStyleBackColor = true;
+            // 
+            // rbLoan
+            // 
+            rbLoan.AutoSize = true;
+            rbLoan.Location = new Point(0, 43);
+            rbLoan.Name = "rbLoan";
+            rbLoan.Size = new Size(86, 27);
+            rbLoan.TabIndex = 10;
+            rbLoan.TabStop = true;
+            rbLoan.Text = "Кредит";
+            rbLoan.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(9F, 23F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.AliceBlue;
+            ClientSize = new Size(900, 518);
+            Controls.Add(groupBox1);
             Controls.Add(btnSave);
             Controls.Add(btnCalculate);
             Controls.Add(lblResult);
@@ -131,8 +191,12 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            Font = new Font("Segoe UI", 10F);
             Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -148,5 +212,9 @@
         private Label lblResult;
         private Button btnCalculate;
         private Button btnSave;
+        private GroupBox groupBox1;
+        private RadioButton rbSimple;
+        private RadioButton rbLoan;
+        private RadioButton rbCompound;
     }
 }
--- BankCalculator/BankCalculator/Form1.cs
+++ BankCalculator/BankCalculator/Form1.cs
@@ -1,4 +1,7 @@
+using System;
 using System.IO;
+using System.Windows.Forms;
+
 namespace BankCalculator
 {
     public partial class Form1 : Form
@@ -8,62 +11,102 @@ namespace BankCalculator
             InitializeComponent();
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void btnCalculate_Click(object sender, EventArgs e)
         {
-            
-            double principal, rate, months;
-
-            
-            if (!double.TryParse(txtAmount.Text, out principal) || principal <= 0)
+            // Перевірка правильності введених даних
+            if (!double.TryParse(txtAmount.Text, out double principal) || principal <= 0)
             {
                 MessageBox.Show("Помилка в полі 'Сума'!\nВведіть додатне число (більше 0).", "Увага");
                 return; 
             }
 
-            
-            if (!double.TryParse(txtRate.Text, out rate) || rate < 0)
+            if (!double.TryParse(txtRate.Text, out double rate) || rate < 0)
             {
-                MessageBox.Show("Помилка в полі 'Відсоток'!\nСтавка не може бути меншою за 0.", "Увага");
+               MessageBox.Show("Помилка в полі 'Відсоток'!\nСтавка не може бути меншою за 0.", "Увага");
+               return;
+            }
+
+            if (!int.TryParse(txtTerm.Text, out int months) || months <= 0)
+            {
+                MessageBox.Show("Помилка в полі 'Термін'!\nВведіть цілу кількість місяців (більше 0).", "Увага");
                 return;
             }
 
-           
-            if (!double.TryParse(txtTerm.Text, out months) || months <= 0)
+            string resultText = "";
+            double resultAmount = 0;
+
+            // 1. РОЗРАХУНОК КРЕДИТУ
+            if (rbLoan.Checked) 
             {
-                MessageBox.Show("Помилка в полі 'Термін'!\nВведіть кількість місяців (більше 0).", "Увага");
+                double monthlyRate = (rate / 100) / 12;
+                double monthlyPayment = principal * (monthlyRate * Math.Pow(1 + monthlyRate, months)) / (Math.Pow(1 + monthlyRate, months) - 1);
+                
+                resultAmount = monthlyPayment * months; 
+                double overpayment = resultAmount - principal; 
+
+                resultText = $"Тип: Кредит (Ануїтет)\n" +
+                             $"Щомісячний платіж: {monthlyPayment:N2} грн\n" +
+                             $"Переплата банку: {overpayment:N2} грн\n" +
+                             $"Загальна сума до повернення: {resultAmount:N2} грн";
+            }
+            // 2. ДЕПОЗИТ - ПРОСТІ ВІДСОТКИ
+            else if (rbSimple.Checked)
+            {
+                double profit = principal * (rate / 100) * ((double)months / 12);
+                resultAmount = principal + profit;
+
+                resultText = $"Тип: Депозит (Прості відсотки)\n" +
+                             $"Чистий прибуток: {profit:N2} грн\n" +
+                             $"Загальна сума на кінець: {resultAmount:N2} грн";
+            }
+            // 3. ДЕПОЗИТ - СКЛАДНІ ВІДСОТКИ
+            else if (rbCompound.Checked)
+            {
+                double monthlyRate = (rate / 100) / 12;
+                resultAmount = principal * Math.Pow(1 + monthlyRate, months);
+                double profit = resultAmount - principal;
+
+                resultText = $"Тип: Депозит (Складні відсотки, капіталізація)\n" +
+                             $"Чистий прибуток: {profit:N2} грн\n" +
+                             $"Загальна сума на кінець: {resultAmount:N2} грн";
+            }
+            else
+            {
+                MessageBox.Show("Будь ласка, оберіть тип розрахунку (Кредит або Депозит).", "Увага");
                 return;
             }
 
-            
-            double result = principal * (1 + (rate / 100) * (months / 12));
-
-            lblResult.Text = $"Загальна сума: {result:N2} грн";
+            // Вивід результату на екран
+            lblResult.Text = resultText;
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            
+            // Перевіряємо, чи є що зберігати
+            if (string.IsNullOrWhiteSpace(lblResult.Text) || lblResult.Text == "lblResult")
+            {
+                MessageBox.Show("Спочатку виконайте розрахунок!", "Увага");
+                return;
+            }
+
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             saveFileDialog.Filter = "Текстовий файл|*.txt";
-            saveFileDialog.Title = "Зберегти звіт";
+            saveFileDialog.Title = "Зберегти банківський звіт";
+            saveFileDialog.FileName = "BankReport.txt";
 
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
-                
-                string report = $"Сума: {txtAmount.Text}\n" +
-                                $"Ставка: {txtRate.Text}%\n" +
-                                $"Срок: {txtTerm.Text} міс.\n" +
+                // Формуємо красивий текст для файлу
+                string report = $"=== БАНКІВСЬКИЙ ЗВІТ ===\n\n" +
+                                $"Початкова сума: {txtAmount.Text} грн\n" +
+                                $"Річна ставка: {txtRate.Text}%\n" +
+                                $"Термін: {txtTerm.Text} міс.\n" +
                                 $"---------------------------\n" +
-                                $"{lblResult.Text}";
+                                $"{lblResult.Text}\n" +
+                                $"===========================";
 
-               
                 File.WriteAllText(saveFileDialog.FileName, report);
-                MessageBox.Show("Файл успішно збережено!");
+                MessageBox.Show("Файл успішно збережено!", "Успіх");
             }
         }
     }
